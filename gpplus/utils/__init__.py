4fa98321db078b7180f5b1f778f2b4eaae7827fa
Kian <77707119+kianswarehouse@users.noreply.github.com>
2025-09-26 13:50:55 -0700

    Update __init__.py

--- gpplus/utils/__init__.py
+++ gpplus/utils/__init__.py
@@ -1,5 +1,4 @@
+from .encoders import MatrixEncoder, NeuralEncoder
 from .input_transform_net import InputTransformNet
-from .standard_scaler import StandardScaler
 from .set_seed import set_seed
 from .standard_scaler import StandardScaler
-from .encoders import MatrixEncoder, NeuralEncoder
