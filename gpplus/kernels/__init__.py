961333deb08832a1566fa11e8d2ab559330bae6a
Tyler Johnson <trjohns1@uci.edu>
2025-10-07 13:50:42 -0700

    updated init to be consistent with this version of the code

--- gpplus/kernels/__init__.py
+++ gpplus/kernels/__init__.py
@@ -10,16 +10,11 @@ from .advanced_kernels import (
     SinhKernel,
 )
 from .unconstrained_kernel import UnconstrainedKernel
-from .factory import KernelFactory, KernelType
 from .gaussian_kernel import GaussianKernel
 from .kronecker import KroneckerKernel
 from .power_exponential_kernel import (
     PowerExponentialKernel,
     PowerExponentialKernelFixed,
 )
-from .combined_kernel_mvmf import CombinedKernel_MVMF
-from .latent_kernel import LatentKernel
-from .hybrid_kernel import HybridKernel
-from .mf_seek_kernel import MF_SEEK_Kernel
 from .process_variance_kernel import ProcessVarianceKernel
 from ..utils.one_hot_to_latent_nn import OneHotToLatent
